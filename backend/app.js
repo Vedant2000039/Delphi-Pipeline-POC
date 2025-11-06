e0ee9754553e0f0a67417c5273ed2ebbb214c89d
VedantMulherkar <vmulherkar@xtsworld.in>
2025-11-06 11:49:40 +0530

    change in app.js

--- backend/app.js
+++ backend/app.js
@@ -34,6 +34,6 @@ const PORT = process.env.PORT || 5000;
 const ENVIRONMENT = process.env.ENVIRONMENT || "dev";
 
 app.listen(PORT, '0.0.0.0', () => {
-  console.log(`🚀 Server running on port ${PORT} [${ENVIRONMENT}]`);
+  console.log(` Server running on port ${PORT} [${ENVIRONMENT}]`);
 });
 
